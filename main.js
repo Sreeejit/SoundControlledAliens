68ef0c30a98142252c0f2298df8e1a595e52b2cf
Sreeejit <77166984+Sreeejit@users.noreply.github.com>
2021-08-23 11:04:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // https://teachablemachine.withgoogle.com/models/hG6bJ4Z8I/
+function startClassification()
+{
+    console.log("'Sir' we have reached function startClassification!")
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/hG6bJ4Z8I/model.json', modelReady);
+
+}
+
+
+function modelReady(){
+    console.log("'Sir' we have reached function modelReady!")
+    classifier.classify(gotResults);
+}
+
+function gotResults(){
+ console.log("'Sir' we have reached the final desitnation the function gotResults!")
+}
